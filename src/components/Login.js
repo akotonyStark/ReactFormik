9a7847e777106c85a63a257b2a937800d228b1a1
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-09-28 12:55:13 +0000

    reducing boilerplate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,56 @@
 import React from 'react'
-import { Formik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 function login() {
   return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={(values) => {
-        const errors = {}
-        if (!values.email) {
-          errors.email = 'Required'
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address'
-        }
-        return errors
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
-          setSubmitting(false)
-        }, 400)
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            style={{ height: '30px', width: '280px' }}
-            type='email'
-            name='email'
-            placeholder='Enter email'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          <br />
-          {errors.email && touched.email && errors.email}
-          <input
-            style={{ height: '30px', width: '280px' }}
-            type='password'
-            name='password'
-            placeholder='Enter password'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          <br />
-          {errors.password && touched.password && errors.password}
-          <button type='submit' disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
+    <div>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {}
+          if (!values.email) {
+            errors.email = 'Required'
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field
+              type='email'
+              name='email'
+              placeholder='Enter email'
+              style={{ height: '30px', width: '280px' }}
+            />
+            <br />
+            <ErrorMessage name='email' component='div' />
+
+            <Field
+              type='password'
+              name='password'
+              placeholder='Enter password'
+              style={{ height: '30px', width: '280px' }}
+            />
+            <br />
+            <ErrorMessage name='password' component='div' />
+
+            <button type='submit' disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   )
 }
 
